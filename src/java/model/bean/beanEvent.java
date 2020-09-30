a0f4b507ab1e91cbfee094d79cac45be3ece67fd
charlene19 <cda611@AFPA-PARIS-DI>
2020-09-30 11:40:24 +0200

    jspEvent

---
+++ src/java/model/bean/beanEvent.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import classes.Book;
+import classes.Event;
+import java.beans.*;
+import java.io.Serializable;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.naming.NamingException;
+import servlets.eventServlet;
+
+/**
+ *
+ * @author cda611
+ */
+public class beanEvent implements Serializable {
+    
+    
+    
+    public beanEvent() {
+      
+    }
+    
+    public List<Book> returnlBook() {
+        
+        List eventList = new ArrayList(); 
+        try { 
+            eventList =  new eventServlet().getList();
+        } catch (SQLException ex) {
+            Logger.getLogger(beanEvent.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (NamingException ex) {
+            Logger.getLogger(beanEvent.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return eventList;
+    }
+    
+    public double getRemise(){
+       //doit retourner ou une remise ou le prix du livre à déterminer. 
+        
+        return 0.0;
+    }
+    
+    public Event getEvent(){
+        
+        return new Event();
+    }
+}
