a839682b15c82a1d7550c01882165f99dd480d45
charlene19 <charlene.romain@yahoo.fr>
2020-10-04 17:54:24 +0200

    jspEvent operationnel

--- src/java/model/bean/beanOrder.java
+++ src/java/model/bean/beanOrder.java
@@ -7,50 +7,74 @@ package model.bean;
 
 import model.entity.Book;
 import model.entity.Order;
-import model.dao.PublisherDAO;
-import model.dao.VatDAO;
-import java.beans.*;
 import java.io.Serializable;
-import static java.lang.System.out;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.naming.InitialContext;
 import javax.naming.NamingException;
-import javax.sql.DataSource;
+import model.dao.BookDAO;
+import model.dao.OrderDAO;
 
 /**
  *
  * @author cda611
  */
 public class beanOrder implements Serializable {
-    
-   
-    
-    public beanOrder() {    
+
+    List<Book> books;
+
+    public beanOrder() {
+      
     }
-    
-    public boolean validateOrder(){
+
+    public boolean validateOrder() {
         //doit retourner un booléen pour valider la commande
         return false;
     }
-    
-    public Order getOrder(){
-        
-        return new Order(); 
+
+    public Order getOrder() {
+
+        return new Order();
+    }
+
+    public List<Book> orderLastMonth(Long id) throws SQLException, NamingException {
+
+        books = new OrderDAO().getListLast(id);
+        return books;
+
+    }
+
+    public List<Book> orderSixMonth(Long id) throws SQLException, NamingException {
+
+        books = new OrderDAO().getListSixMonth(id);
+        return books;
+
+    }
+
+    public List<Book> pastOrder(Long id) throws SQLException, NamingException {
+
+        books = new OrderDAO().getListPast(id);
+        return books;
+
+    }
+
+    public List<Book> allOrder(Long id) throws SQLException, NamingException {
+ id = 2l;
+        books = new OrderDAO().getList(id);
+        return books;
+
     }
     
-    public List allOrder() throws SQLException{
-     
-        return new ArrayList<>();
-      
+    public List<Book> getListById(String id) throws SQLException, NamingException{
+        books = new OrderDAO().getListById(id); 
+        return books;
     }
-    
-    public List unachieveOrder(){
-        
+
+    public List unachieveOrder() {
+
         //retourne les commandes n'ayant pas un statut livré du client
-        return new ArrayList<Order>(); 
+        return new ArrayList<Order>();
     }
+    
+    
 }
