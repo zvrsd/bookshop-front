cc5622c47b610574982bc736a405db3e41ca0216
Charlene19 <70486853+Charlene19@users.noreply.github.com>
2020-10-08 18:32:51 +0200

    advance search

--- src/java/controller/advSearchServlet.java
+++ src/java/controller/advSearchServlet.java
@@ -49,16 +49,29 @@ public class advSearchServlet extends HttpServlet {
             beanSearch beanSearch = new beanSearch(); 
             //List<Book> books = new ArrayList(); 
             Set<Set<Book>> recueil = new HashSet(); 
-            recueil.add( beanSearch.getByTitle((String) request.getParameter("title"))); 
-            recueil.add(beanSearch.getByCategory((String) request.getParameter("category")));
-            recueil.add(beanSearch.getByISBN((String) request.getParameter("isbn")));
-            recueil.add(beanSearch.getByKeywords((String) request.getParameter("keywords")));
+            
+            String title = (String) request.getParameter("title"); 
+            if ((title != null) && (title.length() > 1)){
+            recueil.add( beanSearch.getByTitle(title)); }
+                String category = (String) request.getParameter("category");
+                if (!category.equals("category")){
+            recueil.add(beanSearch.getByCategory(category));}
+                    String isbn = (String) request.getParameter("isbn");
+                    if((isbn != null) && (isbn.length() > 1)){
+            recueil.add(beanSearch.getByISBN(isbn));}
+                        String keyword = (String) request.getParameter("keywords"); 
+                        if ((keyword != null) && (keyword.length() >1)){
+            recueil.add(beanSearch.getByKeywords(keyword));}
            /* if(request.getParameter("priceMin") != null) {
                   if (request.getParameter("priceMax") != null){
             Double min = Double.parseDouble(request.getParameter("priceMin")); 
             Double max = Double.parseDouble(request.getParameter("priceMax"));
              recueil.add( beanSearch.getByPrice(min , max));
             }*/
+                        
+                    
+                
+            
             
             recueil.stream().forEach((book) -> {
                 book.stream().forEach((boo) -> {
